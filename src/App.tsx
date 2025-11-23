0f11bde4f3014670c4f9de371fccd433e8d0b889
syunsuke-I <odol@ymail.ne.jp>
2025-11-23 11:52:37 +0900

    Fix UI

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,10 @@ C D E F | G A B c |`);
       <AbcEditor value={abc} onChange={setAbc} />
 
       {/* 右側: プレビュー (モバイル: 上部1/3) */}
-      <div className="w-full md:w-1/2 h-1/3 md:h-full overflow-auto p-4 md:p-6" style={{ backgroundColor: '#161616' }}>
-        <AbcPreview abc={abc} />
+      <div className="w-full md:w-1/2 h-1/3 md:h-full flex flex-col p-4" style={{ backgroundColor: '#161616' }}>
+        <div className="w-full h-full rounded-lg border border-slate-600 overflow-auto p-4" style={{ backgroundColor: '#1a1a1a' }}>
+          <AbcPreview abc={abc} />
+        </div>
       </div>
     </div>
   );
